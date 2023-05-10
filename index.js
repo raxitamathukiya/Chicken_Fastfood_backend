0b808d514203c970f6586eaecf2dcff1c5510a10
Raxita Mathukiya <jay343421@gmail.com>
2023-05-10 22:37:34 +0530

    10 may 10:37

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ require ('dotenv').config()
 const app=express()
 app.use(cors())
 app.use(express.json())
+app.use(function(req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
+ });
 app.use("/users",userRoute)
 app.use("/product",productRoute)
 app.use(auth)
